81cc2f2659f8860af00617172bd95e0678c1974a
basanti7 <basantistore7@gmail.com>
2023-10-10 13:27:27 +0600

    payment page incomplete

--- shopdue/urls.py
+++ shopdue/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 from shopdue import views
 
 urlpatterns = [
-    path('bill/details/<str:invoice_no>', views.detailOfSingleBill, name='single_bill_detail')
+    path('bill/details/<str:invoice_no>', views.detailOfSingleBill, name='single_bill_detail'),
+    path('bill/payment', views.showPaymentPage, name='show_payment_page')
 ]
--- shopdue/views.py
+++ shopdue/views.py
@@ -3,14 +3,41 @@ import json
 from django.shortcuts import render
 from django.shortcuts import redirect
 from shopdue import models
+from django.db.models import Sum
 # Create your views here.
 
 
 def detailOfSingleBill(request, invoice_no):
     query = request.GET.get('query')
     print("query string is " + str(query))
-    purchase_list = models.Purchase.objects.filter(bill='26349aaa-8dd2-4e84-a554-f141a4218e0c')
+    purchase_list = models.Purchase.objects.filter(
+        bill='26349aaa-8dd2-4e84-a554-f141a4218e0c')
     for itt in purchase_list:
-        print('item - ' + itt.item + " .... Price - " + str(itt.price) + '-- total == ' + str(itt.total))
+        print('item - ' + itt.item + " .... Price - " +
+              str(itt.price) + '-- total == ' + str(itt.total))
+    return HttpResponse("Detailed bill returned successfully invoice --- " + str(invoice_no))
 
-    return HttpResponse("Detailed bill returned successfully invoice --- " + str(invoice_no) )
+
+def showPaymentPage(request):
+    data = {
+        'page_title': 'Payment'
+    }
+    profile_id = request.GET.get('id')
+    customer = None
+    try:
+        customer = models.Customer.objects.get(id=profile_id)
+        # Replace 'YourModel' with the name of your model
+        # 'id' is the field you want to use for the lookup, replace it if necessary
+    except models.Customer.DoesNotExist:
+        # Handle the case where the object with the specified 'id' does not exist
+        customer = None
+    print(customer)
+    customer.total_due = models.Bill.objects.filter(customer=customer).aggregate(tot_due = Sum('due'))['tot_due']
+    print(customer.total_due)
+    # cust.total_due = models.Bill.objects.filter(customer=cust).aggregate(tot_due = Sum('due'))['tot_due']
+    #     if cust.total_due is not None:
+    #         shop_due_total += cust.total_due
+    data['total_due'] = customer.total_due
+
+    print('profile id is : ' + str(profile_id))
+    return render(request, 'payment.html', data)
